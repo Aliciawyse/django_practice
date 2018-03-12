03e46a31524fcb5902b1bcb1593f4d1cd0cfa8e2
Alicia <alicia.barrett89@gmail.com>
2018-03-11 23:30:48 -0400

    Getting to know views and templates

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 # the . before models current directory
 from .models import Post
 
+from django.shortcuts import render, get_object_or_404
+
+
 
 
 # Create your views here.
@@ -12,3 +15,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
